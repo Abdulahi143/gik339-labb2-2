da67b30ec68d0e5e1922ae2a962a7a7a76b0c127
Abdulahi143 <qaskaxaji143@gmail.com>
2025-12-01 14:17:33 +0100

    yeah

---
+++ script.js
@@ -0,0 +1,17 @@
+const checkBox = document.getElementById('divStyle');
+const textField = document.getElementsByClassName('textfield');
+const btn = document.getElementById('fun-button');
+const divElement = document.getElementById('form-section');
+
+
+
+btn.addEventListener('click', () => {
+    alert('Grattis! Du tryckte på den mest meningslösa knappen i universum 😄');
+});
+
+
+function inputListener( ) {
+    
+}
+
+
